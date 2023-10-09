5840d537184d7e560431ef8ac05a233d35d6226e
jorgefdc <jorgefdc@outlook.com>
2023-10-09 19:02:37 +0100

    Identention adjusted

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,23 @@
 package org.example;
 
+import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        double[] values = new double[] {65, 51 , 16, 11 , 6519, 191 ,0 , 98, 19854, 1, 32};
+        DescriptiveStatistics descriptiveStatistics = new DescriptiveStatistics();
+        for (double v : values) {
+            descriptiveStatistics.addValue(v);
+        }
+        double mean = descriptiveStatistics.getMean();
+        double median = descriptiveStatistics.getPercentile(50);
+        double standardDeviation = descriptiveStatistics.getStandardDeviation();
+
+        for (double value : values) {
+            System.out.println(value);
+        }
+
+        System.out.println("\nMean: " + mean + "\nMedian: " + median +
+                "\nStandard deviation: " + standardDeviation);
     }
 }
